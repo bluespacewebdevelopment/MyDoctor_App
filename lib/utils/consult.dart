20821546112b11e0ad0221af55ab4716a83a7664
Bluespace <108890461+bluespacewebdevelopment@users.noreply.github.com>
2022-08-05 20:27:15 +0530

    Add files via upload

---
+++ lib/utils/consult.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Consult extends StatefulWidget {
+  const Consult({Key? key}) : super(key: key);
+
+  @override
+  State<Consult> createState() => _ConsultState();
+}
+
+class _ConsultState extends State<Consult> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
